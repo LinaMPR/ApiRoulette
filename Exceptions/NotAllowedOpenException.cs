7ff880f54431d522380796e4c142009e74d2a149
Lina Maria <linapaez96@gmail.com>
2020-11-05 13:16:29 -0500

    Create Exceptions folder and files. Edit RouletteService.cs file

---
+++ Exceptions/NotAllowedOpenException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiRoulette.Exceptions
+{
+    public class NotAllowedOpenException : Exception
+    {
+    }
+}
--- Services/RouletteService.cs
+++ Services/RouletteService.cs
@@ -1,4 +1,6 @@
-﻿using ApiRoulette.Models;
+﻿using ApiRoulette.Exceptions;
+using ApiRoulette.Models;
+using ApiRoulette.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,43 +10,87 @@ namespace ApiRoulette.Services
 {
     public class RouletteService : IRouletteService
     {
+        private IRouletteRepository rouletteRepository;
         public Roulette Bet(string Id, string UserId, int position, double money)
         {
-            try
+            if(money > 10000 || money < 1)
             {
-                if (money > 10000 || money < 1)
-                {
-                    //throw new CashOutRangeException();
-                }
-            } catch (Exception e)
-            {
-
+                throw new CashOutRangeException();
             }
+            Roulette roulette = rouletteRepository.GetById(Id);
+            if(roulette == null)
+            {
+                throw new RouletteNotFound();
+            }
+            if (roulette.IsOpen == false)
+            {
+                throw new RouletteClosesException();
+            }
+
+            double value = 0d;
+            roulette.board[position].TryGetValue(UserId, out value);
+            roulette.board[position].Remove(UserId + "");
+            roulette.board[position].TryAdd(UserId + "", value + money);
+
+            return rouletteRepository.Update(roulette.Id, roulette);
         }
 
         public Roulette Close(string Id)
         {
-            throw new NotImplementedException();
+            Roulette roulette = rouletteRepository.GetById(Id);
+            if (roulette == null)
+            {
+                throw new RouletteNotFound();
+            }
+            if(roulette.ClosedAt != null)
+            {
+                throw new NotAllowedClosedException();
+            }
+            roulette.ClosedAt = DateTime.Now;
+            roulette.IsOpen = false;
+
+            return rouletteRepository.Update(Id, roulette);
         }
 
         public Roulette create()
         {
-            throw new NotImplementedException();
+            Roulette roulette = new Roulette()
+            {
+                Id = Guid.NewGuid().ToString(),
+                IsOpen = false,
+                OpendAt = null,
+                ClosedAt = null
+            };
+            rouletteRepository.Save(roulette);
+
+            return roulette;
         }
 
         public Roulette Find(string Id)
         {
-            throw new NotImplementedException();
+            return rouletteRepository.GetById(Id);
         }
 
         public List<Roulette> GetAll()
         {
-            throw new NotImplementedException();
+            return rouletteRepository.GetAll();
         }
 
         public Roulette Open(string Id)
         {
-            throw new NotImplementedException();
+            Roulette roulette = rouletteRepository.GetById(Id);
+            if(roulette == null)
+            {
+                throw new RouletteNotFound();
+            }
+            if(roulette.OpendAt != null)
+            {
+                throw new NotAllowedOpenException();
+            }
+            roulette.OpendAt = DateTime.Now;
+            roulette.IsOpen = true;
+
+            return rouletteRepository.Update(Id, roulette);
         }
     }
 }
