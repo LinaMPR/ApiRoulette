9db25e919be442c217bcd96b81e7695a8cbfd5f2
Lina Maria <linapaez96@gmail.com>
2020-11-05 09:27:23 -0500

    Create binder Models and archive Roulette.cs, configurate Program.cs, Starup.cs

---
+++ Models/Roulette.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Routing.Constraints;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiRoulette.Models
+{
+    [Serializable]
+    public class Roulette
+    {
+        public string Id { get; set; }
+        public bool IsOpen { get; set; }
+        public DateTime? OpendAt { get; set; }
+        public DateTime? ClosedAt { get; set; }
+        public IDictionary<string, double>[] board { get; set; } = new IDictionary<string, double>[39];
+        public Roulette()
+        {
+            this.Init();
+        }
+
+        private void Init()
+        {
+            for(int i =0; i < board.Length; i++)
+            {
+                board[i] = new Dictionary<string, double>();
+            }
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,15 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using EasyCaching.Core.Configurations;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace ApiRoulette
 {
@@ -26,6 +20,15 @@ namespace ApiRoulette
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddEasyCaching(options =>
+            {
+                options.UseRedis(redisConfig =>
+                {
+                    redisConfig.DBConfig.Endpoints.Add(new ServerEndPoint("redesdb", 6379));
+                    redisConfig.DBConfig.AllowAdmin = true;
+                },
+                "roulette");
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
