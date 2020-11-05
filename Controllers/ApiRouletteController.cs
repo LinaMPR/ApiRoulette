745e928677651b4d0c6b5a49b4f2da4587933c90
Lina Maria <linapaez96@gmail.com>
2020-11-05 12:48:18 -0500

    Edition the ApiRouletteControl.cs file and create a DTO folder with the BetRequest.cs file.

--- Controllers/ApiRouletteController.cs
+++ Controllers/ApiRouletteController.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
+using ApiRoulette.DTO;
+using ApiRoulette.Models;
+using ApiRoulette.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -11,6 +15,91 @@ namespace ApiRoulette.Controllers
     [Route("[controller]")]
     public class ApiRouletteController : ControllerBase
     {
-        
+        private IRouletteService rouletteService;
+
+        public ApiRouletteController(IRouletteService rouletteService)
+        {
+            this.rouletteService = rouletteService;
+        }
+
+        //<sumary>
+        //Creation New Rulette
+        //</sumary>
+        [HttpPost]
+        public IActionResult NewRulette()
+        {
+            Roulette roulette = rouletteService.create();
+            return Ok(roulette);
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(rouletteService.GetAll());
+        }
+
+        //<sumary>
+        //Open Roulette Id
+        //</sumary>
+        [HttpPut("{id}/open")]
+        public IActionResult Open([FromRoute(Name = "id")] string id)
+        {
+            try
+            {
+                rouletteService.Open(id);
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return StatusCode(405);
+            }
+        }
+
+        //<sumary>
+        //Close Best on a Rulette
+        //</sumary>
+        [HttpPut("{id}/close")]
+        public IActionResult Close([FromRoute(Name ="id")]string id)
+        {
+            try
+            {
+                Roulette roulette = rouletteService.Close(id);
+                return Ok(roulette);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return StatusCode(405);
+            }
+        }
+        //<sumary>
+        //Lets place a bet
+        //</sumary>
+        [HttpPost("{id}/bet")]
+        public IActionResult Bet([FromHeader(Name ="user-id")] string UserId,[FromRoute(Name = "id")] 
+                                            string id, [FromBody] BetRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(new
+                {
+                    error = true,
+                    msg = "Bad Request"
+                });
+            }
+
+            try
+            {
+                Roulette roulette = rouletteService.Bet(id, UserId, request.position, request.money);
+                return Ok(roulette);
+            } 
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return StatusCode(405);
+            }
+        }
+
     }
 }
---
+++ DTO/BetRequest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiRoulette.DTO
+{
+    public class BetRequest
+    {
+        //<sumary>
+        //Position 0-36, and 37=>red, 38=>black
+        //</sumary>
+        [Range(0,38)]
+        public int position { get; set; }
+
+        //<sumary>
+        //Money to Bet
+        //</sumary>
+        [Range(0.5d, maximum: 100000)]
+        public double money { get; set; }
+    }
+}
